08d8487f554267df2ed3dfbfd41fb52b28d46be9
Jason Hawks <jason@netbit.me>
2015-02-14 06:40:25 -0500

    added hifi fixed rtmidi x2

---
+++ rtmidi-hifi.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Rtmidi < Formula
+  homepage "http://www.music.mcgill.ca/~gary/rtmidi/"
+  url "http://hifi-public.s3.amazonaws.com/dependencies/rtmidi-2.1.0.tar.gz"
+  sha1 "e6e8fe7f67eb6dbf0504f72307a47a41e06b1652"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    
+    system "make"
+    lib.install Dir['*.a', '*.dylib']
+    include.install Dir['*.h']
+  end
+end
